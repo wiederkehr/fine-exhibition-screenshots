fc981b23a0e82beacdf458e0fa2eef0420b10cd7
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-06-07 11:44:23 +0200

    Better image naming and serving.

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const app = express()
 const Nightmare = require('nightmare');
-const fs = require('fs')
+
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
 
+  var detailId  = req.query.detailId;
+  var imageName = detailId ? 'static_emotion_' + detailId : 'static_emotion';
+  var imagePath = 'public/images/' + imageName + '.png';
   var nightmare = Nightmare({height: 600, width: 1200});
 
   nightmare
     .goto('http://fine-exhibition.annawiederkehr.com/detail')
-    .screenshot('screenshots/screenshot.png')
-    .end()
+    .screenshot(imagePath)
+    .end(() => {
+
+      // res.setHeader('Content-Type', 'application/json');
+      // res.send(JSON.stringify({ url: __dirname + '/' + imagePath }));
+
+      res.setHeader('Content-Type', 'image/png');
+      res.sendFile(imageName + '.png', {root: __dirname + '/public/images/'});
+
+    })
     .catch(function (error) {
       console.error('Screenshot failed:', error);
     });
-  res.sendFile('screenshot.png', {root: __dirname + '/screenshots/'})
+
 })
 
 app.listen(3000, function () {
